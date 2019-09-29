4638681bac227794b9ac11dfba0134552bc16bec
Harvinder Singh <harvinder.singh@ashling.com>
2019-09-29 23:59:28 +0530

    Updates

---
+++ precache-manifest.ae12822a420136ab4a05d2d70743a37c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fca34f23834da36401c51d6b95ab555",
+    "url": "/harvin123/React_Online_shopping-App/index.html"
+  },
+  {
+    "revision": "5e7a7d8c6d9df0e3b17e",
+    "url": "/harvin123/React_Online_shopping-App/static/css/2.f1e99808.chunk.css"
+  },
+  {
+    "revision": "49bd69a5619761f14c6c",
+    "url": "/harvin123/React_Online_shopping-App/static/css/main.3f5b4e01.chunk.css"
+  },
+  {
+    "revision": "5e7a7d8c6d9df0e3b17e",
+    "url": "/harvin123/React_Online_shopping-App/static/js/2.5008016b.chunk.js"
+  },
+  {
+    "revision": "49bd69a5619761f14c6c",
+    "url": "/harvin123/React_Online_shopping-App/static/js/main.557a4c8b.chunk.js"
+  },
+  {
+    "revision": "f2dd6502e74ca639bbcd",
+    "url": "/harvin123/React_Online_shopping-App/static/js/runtime~main.898b00a9.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/harvin123/React_Online_shopping-App/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/harvin123/React_Online_shopping-App/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/harvin123/React_Online_shopping-App/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/harvin123/React_Online_shopping-App/static/media/slick.f97e3bbf.svg"
+  }
+]);
